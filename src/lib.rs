357496b192655e86828faa9f65df63252305ef89
uselessgoddess <dodickgod@gmail.com>
2023-08-14 20:29:47 +0300

    Patches to `0.1.0`: - use new build system - add `rustfmt.toml`

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ use {
     proc_macro2::{Ident, Span, TokenStream},
     quote::{format_ident, quote},
     std::{
-        env, fs, io,
+        env, error, fs,
         sync::atomic::{AtomicUsize, Ordering},
     },
     syn::{
@@ -38,12 +38,14 @@ pub fn js_impl(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let ident = format_ident!("__some_prefix_{unique}");
     let Input { sig, block } = parse_macro_input!(input as Input);
 
-    let err: io::Result<_> = try {
-        let path = env::current_dir()?.join("_jsauto_cache");
+    let ret: Result<_, Box<dyn error::Error>> = try {
+        let path =
+            env::current_dir()?.join(env::var("CARGO_PKG_NAME")?).join("template/src/_js_cache");
         let _ = fs::create_dir(&path);
 
+        let cache = path.join(format!("{ident}.cjs"));
         fs::write(
-            path.join(&format!("{ident}.js")),
+            &cache,
             quote!(
                 module.exports.#ident = async function(#sig) {
                     #block
@@ -51,13 +53,16 @@ pub fn js_impl(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
             )
             .to_string(),
         )?;
+
+        cache
     };
 
-    if let Err(err) = err {
-        return Error::new(Span::call_site(), format!("{err}: lmao")).to_compile_error().into();
-    }
-
-    let path = format!("/_jsauto_cache/{ident}.js");
+    let path = match ret {
+        Err(err) => {
+            return Error::new(Span::call_site(), format!("{err}")).to_compile_error().into();
+        }
+        Ok(path) => path.display().to_string(),
+    };
     let (ser, de) = (sig.iter(), sig.iter());
     quote!(
         #[wasm_bindgen(module = #path)]
